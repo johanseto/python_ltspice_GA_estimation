2c160faa20d8ba9f8323aec3c9c9b9450725fbb6
johan.castiblanco <johan.castiblanco@pevaar.com>
2021-01-19 09:29:45 -0500

    full cambios para graficas de CFL con fitness  de convergencia

--- cfl_lamp/plot_file.py
+++ cfl_lamp/plot_file.py
@@ -33,49 +33,31 @@ def plotting(fit_solution):
     
     
     colors = cm.get_cmap('tab10', 10)
-    f, (ax1, ax2) = plt.subplots(2, 1)
-    ax1.plot(simulation_adjust.t, simulation_adjust.i,linewidth=3, color=colors(0), 
+    f, (ax2, ax1) = plt.subplots(2, 1)
+    ax1.plot(simulation_adjust.t*1e3, simulation_adjust.i,linewidth=3, color=colors(0), 
              label=' i-simulada')
-    ax1.plot(measure.t, measure.i,linewidth=3, color=colors(1), label='i- medida')
-    ax1.set_title('Resultados')
-    ax1.set_xlim(0, measure.t[-1])
+    ax1.plot(measure.t*1e3, measure.i,linewidth=3, color=colors(1), label='i- medida')
+    ax2.set_title('Resultados')
+    ax1.set_xlim(0, measure.t[-1]*1e3)
     ax1.set_ylim(min(measure.i)-0.1, max(measure.i)+0.1)
     # ax1.set_xlabel('Iteracion(i)')
-    ax1.set_xlabel(r'Tiempo(s)', labelpad=10)
+    ax1.set_xlabel(r'Tiempo(ms)', labelpad=10)
     ax1.set_ylabel(r'Corriente(A)', labelpad=10)
     # ax1.spines['right'].set_visible(False)
     # ax1.spines['top'].set_visible(False)
-    ax1.xaxis.set_tick_params(which='major', size=10, width=2, direction='in')
-    ax1.xaxis.set_tick_params(which='minor', size=7, width=2, direction='in')
-    ax1.yaxis.set_tick_params(which='major', size=10, width=2, direction='in')
-    ax1.yaxis.set_tick_params(which='minor', size=7, width=2, direction='in')
-    ax1.grid(True)
     
-    ax1.xaxis.set_major_locator(mpl.ticker.MultipleLocator(1/120))
-    ax1.xaxis.set_minor_locator(mpl.ticker.MultipleLocator(1/240))
-    ax1.yaxis.set_major_locator(mpl.ticker.MultipleLocator(max(measure.i)/4  ))
-    ax1.yaxis.set_minor_locator(mpl.ticker.MultipleLocator(max(measure.i)/2  ))
+
     ax1.legend(bbox_to_anchor=(0.78, 0.8), loc=10, frameon=True, fontsize=14)
     
-    ax2.plot(simulation_adjust.t, simulation_adjust.v, linewidth=3,color=colors(0),
+    ax2.plot(simulation_adjust.t*1e3, simulation_adjust.v, linewidth=3,color=colors(0),
              label='v-simulada')
-    ax2.plot(measure.t, measure.v, linewidth=3,color=colors(1),
+    ax2.plot(measure.t*1e3, measure.v, linewidth=3,color=colors(1),
              label='v-medida')
     
-    ax2.set_xlim(0, measure.t[-1])
+    ax2.set_xlim(0, measure.t[-1]*1e3)
     ax2.set_ylim(min(measure.v)-10, max(measure.v)+10)
     ax2.set_ylabel(r'Tension(V)', labelpad=10)
-    ax2.set_xlabel(r'Tiempo(s)', labelpad=10)
-    ax2.xaxis.set_tick_params(which='major', size=10, width=2, direction='in')
-    ax2.xaxis.set_tick_params(which='minor', size=7, width=2, direction='in')
-    ax2.yaxis.set_tick_params(which='major', size=10, width=2, direction='in')
-    ax2.yaxis.set_tick_params(which='minor', size=7, width=2, direction='in')
-    ax2.grid(True)
-    
-    ax2.xaxis.set_major_locator(mpl.ticker.MultipleLocator(1/120))
-    ax2.xaxis.set_minor_locator(mpl.ticker.MultipleLocator(1/240))
-    ax2.yaxis.set_major_locator(mpl.ticker.MultipleLocator(50))
-    ax2.yaxis.set_minor_locator(mpl.ticker.MultipleLocator(25))
+    ax2.set_xlabel(r'Tiempo(ms)', labelpad=10)
     ax2.legend(bbox_to_anchor=(0.78, 0.8), loc=10, frameon=True, fontsize=14)
     
     

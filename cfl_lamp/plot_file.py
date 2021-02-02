e22b4e7b33d0427b9c33c917f6d70c979fbf260e
johan.castiblanco <johan.castiblanco@pevaar.com>
2021-02-01 23:10:40 -0500

    formato de imagnees para tamanio de labels

--- cfl_lamp/plot_file.py
+++ cfl_lamp/plot_file.py
@@ -31,35 +31,38 @@ def plotting(fit_solution):
     dist,measure,simulation_adjust=fitnessCfl(fit_solution , models="true")
 
     
-    
+    nlabel_axis=25
+    nlabel_signal=25
     colors = cm.get_cmap('tab10', 10)
     f, (ax2, ax1) = plt.subplots(2, 1)
     ax1.plot(simulation_adjust.t*1e3, simulation_adjust.i,linewidth=3, color=colors(0), 
-             label=' i-simulada')
-    ax1.plot(measure.t*1e3, measure.i,linewidth=3, color=colors(1), label='i- medida')
-    ax2.set_title('Resultados')
+             label=' i-Simulada propuesta')
+    ax1.plot(measure.t*1e3, measure.i,linewidth=3, color=colors(1), label='i-Medida')
+    ax2.set_title('Resultados',fontsize=nlabel_axis)
     ax1.set_xlim(0, measure.t[-1]*1e3)
     ax1.set_ylim(min(measure.i)-0.1, max(measure.i)+0.1)
     # ax1.set_xlabel('Iteracion(i)')
-    ax1.set_xlabel(r'Tiempo(ms)', labelpad=10)
-    ax1.set_ylabel(r'Corriente(A)', labelpad=10)
+    ax1.set_xlabel(r'Tiempo(ms)',fontsize=nlabel_axis)
+    ax1.set_ylabel(r'Corriente(A)',fontsize=nlabel_axis)
     # ax1.spines['right'].set_visible(False)
     # ax1.spines['top'].set_visible(False)
-    
+    ax1.xaxis.set_tick_params(labelsize=20)
+    ax1.yaxis.set_tick_params(labelsize=20)
 
-    ax1.legend(bbox_to_anchor=(0.78, 0.8), loc=10, frameon=True, fontsize=14)
+    ax1.legend(bbox_to_anchor=(0.78, 0.8), loc=10, frameon=True, fontsize=20)
     
     ax2.plot(simulation_adjust.t*1e3, simulation_adjust.v, linewidth=3,color=colors(0),
-             label='v-simulada')
+             label='v-Simulada  \n propuesta ')
     ax2.plot(measure.t*1e3, measure.v, linewidth=3,color=colors(1),
-             label='v-medida')
+             label='v-Medida')
     
     ax2.set_xlim(0, measure.t[-1]*1e3)
     ax2.set_ylim(min(measure.v)-10, max(measure.v)+10)
-    ax2.set_ylabel(r'Tension(V)', labelpad=10)
-    ax2.set_xlabel(r'Tiempo(ms)', labelpad=10)
-    ax2.legend(bbox_to_anchor=(0.78, 0.8), loc=10, frameon=True, fontsize=14)
-    
+    ax2.set_ylabel(r'Tensión(V)', fontsize=nlabel_axis)
+    ax2.set_xlabel(r'Tiempo(ms)',fontsize=nlabel_axis)
+    ax2.legend(bbox_to_anchor=(0.78, 0.8), loc=10, frameon=True, fontsize=20)
+    ax2.xaxis.set_tick_params(labelsize=20)
+    ax2.yaxis.set_tick_params(labelsize=20)
     
     
     
